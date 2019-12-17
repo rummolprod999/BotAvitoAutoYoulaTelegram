d16fd3efa7209c167f81646ad344ddf589433e52
rummolprod999 <rummolprod999@gmail.com>
2019-12-17 11:06:37 +0300

    fix issues

--- TelegramBot.py
+++ TelegramBot.py
@@ -10,7 +10,7 @@ class TelegramBot:
         phone = f"Телефон: {item.phone}\n" if item.phone else ""
         name = f"Имя: {item.user_name}\n" if item.user_name else ""
         place = f"<i>Местоположение:</i> {item.city}\n" if item.city else ""
-        message = f"""<i>Дата:</i>{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} \n<i>Тип:</i> {item.type_url.type_url}\n<i>Название:</i> <b>{item.title}</b>\n{place}<i>Цена:</i> <b>{item.price}</b>\n{last_price}<i>Описание:</i> {
+        message = f"""<i>Дата:</i> {datetime.now().strftime('%Y-%m-%d %H:%M:%S')} \n<i>Тип:</i> {item.type_url.type_url}\n<i>Название:</i> <b>{item.title}</b>\n{place}<i>Цена:</i> <b>{item.price}</b>\n{last_price}<i>Описание:</i> {
         item.desc}\n{phone}{name}<i>Дата последнего обновления:</i> {item.data}\n<i>Ссылка:</i> {item.url} """
         # print(message)
         Tools.bot.send_message(chat_id=Tools.CHANNEL_NAME, text=message, parse_mode='HTML')
